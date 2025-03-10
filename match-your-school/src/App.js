09e2fae6e56164c0215276c3daac54c090f7478b
Yuhua Hong <u7669815@anu.edu.au>
2025-03-10 16:12:07 +1100

    Add routing and introduction component to App; implement background animation

--- match-your-school/src/App.js
+++ match-your-school/src/App.js
@@ -1,19 +1,42 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
+import Introduction from './components/Introduction.js';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+function HomePage(){
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('introduction');
+    }
+
+    return (
         <div className="container">
             <div className="title">
                 <h1>Elevate Your Potential</h1>
             </div>
-            <button className="startButton">
+            <button className="startButton" onClick={handleClick}>
                 Get Started
             </button>
         </div>
+    );
+}
 
-    </div>
+
+function App() {
+  return (
+    <Router>
+        <div className="App">
+            <Routes>
+                <Route path = "/" element={<HomePage />} />
+                <Route path = "/introduction" element={<Introduction />} />
+
+            </Routes>
+
+        </div>
+
+    </Router>
+    
   );
 }
 
---
+++ match-your-school/src/components/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Introduction() {
+    return(
+        <div className="App">
+            <div className="container">
+                <h1>Explore what’s next for you</h1>
+            </div>
+        </div>
+    )
+
+}
+
+export default Introduction;
