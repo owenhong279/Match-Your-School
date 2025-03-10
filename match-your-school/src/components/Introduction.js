bfc3ebdfa2aab2c41434269590e5e1ddcc10334e
Yuhua Hong <u7669815@anu.edu.au>
2025-03-10 19:49:19 +1100

    Refactor Introduction component styles and fix typo in heading

--- match-your-school/src/components/Introduction.js
+++ match-your-school/src/components/Introduction.js
@@ -10,7 +10,7 @@ function Introduction() {
     return(
         <div className="introduction-page">
             <div className="introduction-container">
-                <h1>Explore what next for you</h1>
+                <h1>Explore what's next for you</h1>
                 <p>Finding the right school and program is a crucial step in shaping your future. By answering a few key questions about your interests, goals, and learning preferences, you’ll receive personalized recommendations that align with your aspirations. Our matching process considers factors like career pathways, academic strengths, and hands-on opportunities to help you make an informed choice with confidence. Whether you seek a specialized program or a well-rounded education, we’re here to guide you toward the best fit for your journey.</p>
                 <button 
                     className='explore'
